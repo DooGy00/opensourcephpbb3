c0d39e167e9ba1fed9487fc06db83ed0101b3166
DooGy00 <drgvd75@gmail.com>
2016-12-01 11:05:45 -0600

    Update slide.js

--- slide.js
+++ slide.js
@@ -1,26 +1,18 @@
 function slideDown(elem) {
 	elem.style.maxHeight = '1000px';
-	// We're using a timer to set opacity = 0 because setting max-height = 0 doesn't (completely) hide the element.
+	
 	elem.style.opacity   = '1';
 }
 
-/**
- * Slide element up (like jQuery's slideUp)
- * @param  {Node} elem Element
- * @return {[type]}      [description]
- */
+
 function slideUp(elem) {
 	elem.style.maxHeight = '0';
-	once( 1, function () {
+	once( 10, function () {
 		elem.style.opacity = '0';
 	});
 }
 
-/**
- * Call once after timeout
- * @param  {Number}   seconds  Number of seconds to wait
- * @param  {Function} callback Callback function
- */
+
 function once (seconds, callback) {
 	var counter = 0;
 	var time = window.setInterval( function () {
@@ -29,5 +21,5 @@ function once (seconds, callback) {
 			callback();
 			window.clearInterval( time );
 		}
-	}, 400 );
+	}, 1000 );
 }
